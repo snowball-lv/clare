6e7ed0565dd83a65518df0e15118563f83f21a3f
ovalters <ovalters@webtogo.de>
2017-09-06 01:42:44 +0300

    added proper deps. for generated files

--- rakefile-gen.rb
+++ rakefile-gen.rb
@@ -1,13 +1,40 @@
 
 
-unless Dir.exist?("gen/")
-    mkdir_p "gen/"
-end
-
 IR = "scripts/ir"
 
 Dir.chdir(IR) do
-    sh "./ir-gen.rb"
+    if ARGV.include? "clean"
+        sh "rake clean"
+    else
+        sh "rake"
+    end
 end
 
-copy_entry("#{IR}/output/", "gen/")
+def make_copy_task(src, dst)
+
+    dir = File.dirname(dst)
+
+    file(dst => dir)
+    directory(dir)
+
+    file(dst => src) do
+        copy(src, dst)
+    end
+
+    task :default => dst
+end
+
+unless ARGV.include? "clean"
+
+    IR_OUT = "#{IR}/output"
+    IR_SRC = FileList["#{IR_OUT}/**/*.*"]
+    IR_DST = IR_SRC.pathmap("%{^#{IR_OUT}/,gen/}p")
+
+    IR_SRC.zip(IR_DST).each do |src, dst|
+        make_copy_task(src, dst)
+    end
+end
+
+task :clean do
+    rm_rf "gen/"
+end
