3ae455e5d34d9b8d2c6746938cf13ab05dd25dca
ovalters <ovalters@webtogo.de>
2017-09-06 04:05:35 +0300

    prototyping some IR nodes

--- dev/ir/ir.c
+++ dev/ir/ir.c
@@ -3,10 +3,24 @@
 #include <mem/Mem.h>
 #include <ir/IR.h>
 
+#include <stdio.h>
+
 int main() {
     assert(MemEmpty());
 
     ir_dummy();
+
+    printf("ir dummy value: %d\n", IR.dummy);
+
+    Tmp *tmp = IR.NewTmp();
+
+    IR.Asgn(
+        tmp,
+        IR.Add(
+            IR.I32(2),
+            IR.I32(3)));
+
+    IR.DeleteTmp(tmp);
 
     assert(MemEmpty());
 	return 0;
---
+++ scripts/ir/ir-binding.rb
@@ -0,0 +1,8 @@
+
+
+class IRBinding
+
+    def get_binding
+        return binding()
+    end
+end
