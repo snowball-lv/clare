08d526b891290cdba52cdc08b648c49dafade8bc
ovalters <ovalters@webtogo.de>
2017-11-16 12:12:41 +0200

    dummy rules for simple addition

--- src/backends/dummy/Dummy.c
+++ src/backends/dummy/Dummy.c
@@ -4,12 +4,15 @@
 #include <collections/List.h>
 
 #include <stdio.h>
+#include <assert.h>
 
 int dummy;
 
 void _DummyMunch(Node *root, List *patterns) {
     
     int _index = 0;
+    
+    #define MUNCH(node)     _DummyMunch(node, patterns);
     
     #define RULE(pattern, action) {             \
         Node *p = ListGet(patterns, _index);    \
@@ -21,6 +24,21 @@ void _DummyMunch(Node *root, List *patterns) {
     }
         #include <backends/dummy/dummy.rules>
     #undef RULE
+    
+    #undef MUNCH
+        
+    printf("Trying to match node: %s\n", NodeName(root));
+    assert(0 && "No rule matches node");
+}
+
+static void DeletePatternTree(Node *root) {
+    for (int i = 0; i < IR_MAX_KIDS; i++) {
+        Node *child = root->kids[i];
+        if (child != 0) {
+            DeletePatternTree(child);
+        }
+    }
+    MemFree(root);
 }
 
 void DummyMunch(Node *root) {
@@ -34,7 +52,7 @@ void DummyMunch(Node *root) {
     _DummyMunch(root, patterns);
     
     LIST_EACH(patterns, Node *, pattern, {
-        MemFree(pattern);
+        DeletePatternTree(pattern);
     });
     DeleteList(patterns);
 }
