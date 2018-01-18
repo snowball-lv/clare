d898cdc125b4af4a8345ac4908c18bc226973af5
ovalters <ovalters@webtogo.de>
2018-01-18 03:10:18 +0200

    split CLIs into their own folders

---
+++ cli/clare-f1/main.c
@@ -0,0 +1,10 @@
+#include <helpers/Unused.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+    UNUSED(argc);
+    UNUSED(argv);
+    printf("--- Clare F1 CLI ---\n");
+    return 0;
+}
