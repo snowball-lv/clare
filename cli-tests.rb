e342f6536b502eae33c5740d3d6af124501cb303
ovalters <ovalters@webtogo.de>
2018-02-02 15:36:09 +0200

    f1 tokenizer

---
+++ cli-tests.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+
+Dir.chdir("bin") do
+    system "./clare-f1 ../cli-tests/example.f1"
+end
--- frontends/f1/src/main.c
+++ frontends/f1/src/main.c
@@ -1,14 +1,32 @@
-
-
 #include <dummy.h>
 #include <stdio.h>
 
 #include <clare/helpers/Unused.h>
+#include <clare/helpers/Error.h>
+#include <clare/collections/List.h>
+#include <clare/regex/RegEx.h>
+#include <clare/mem/Mem.h>
 
+#define RULE_FILE   <lexer.rules>
+    #include <clare/regex/lexer.def>
+#undef RULE_FILE
 
 int main(int argc, char **argv) {
     UNUSED(argc);
     UNUSED(argv);
     printf("--- Clare F1 CLI ---\n");
+    
+    for (int i = 1; i < argc; i++) {
+        const char *path = argv[i];
+        printf("input file: %s\n", path);
+        FILE *file = fopen(path, "r");
+        ASSERT(file != 0);
+        
+        Lex(file);
+        
+        fclose(file);
+    }
+    
     return 0;
 }
+
