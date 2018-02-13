229c04040cd89839a15803a852cd1533b1ff803a
ovalters <ovalters@webtogo.de>
2018-02-13 16:23:43 +0200

    modified loc to include back/front ends

--- loc.sh
+++ loc.sh
@@ -2,7 +2,11 @@
 
 {                               \
     find inc/ -name "*.*" &     \
-    find src/ -name "*.*";      \
+    find src/ -name "*.*" &      \
+    find backends/*/inc -name "*.*" &      \
+    find backends/*/src -name "*.*" &      \
+    find frontends/*/inc -name "*.*" &      \
+    find frontends/*/src -name "*.*" &      \
 } |                             \
 xargs wc -l
 
